df5787cf4a487bf86620ed0ba07924c3363f8ae4
Abdug'affor Abdurahimov <abdugaffor.abdurahimov@inoviagroup.se>
2021-11-18 10:07:10 +0500

    Updated

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,45 @@
  * 
  * ---- NO Side Effects ----
  * No Observable effects besides computing a value
+ *  
  * 
+ * --> Same input same output
  */
 
 
-var xs = [1,2,3,4,5]
 
-console.log(xs.splice(0, 3));
-console.log(xs.splice(0, 3));
-console.log(xs.splice(0, 3));
+const add = (x: number, y: number) => x + y
+
+
+// console.log(add(1,2));
+// console.log(add([1,2]));
+
+
+// const toPair = (f: Function) => 
+//     ([x, y]) => f(x, y)
+
+// const fromPair = (f: Function) => 
+//     (x, y) => f([x, y])
+
+// const result = fromPair(toPair(add))(1,2)
+
+// console.log(result);
+
+// const flip = (f: Function) => (y, x) => f(x, y)
+
+// const result = flip(add)(1,2)
+
+// console.log(result)
+
+
+const curry = (f: Function) => x => y => f(x, y)
+const unCurry = (f:Function) => (x, y) => f(x)(y)
+
+const curryiedAdd = curry(add)
+
+const increment = curryiedAdd(1)
+
+const result = increment(2)
+
+console.log(result);
+    
